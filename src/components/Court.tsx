9ac7870c5192cc3a9dd836afb1f8fcd756737c3d
nakashi94 <n0904yBUNJOU@gmail.com>
2022-10-15 04:02:32 +0900

    change layout

--- src/components/Court.tsx
+++ src/components/Court.tsx
@@ -5,7 +5,7 @@ import { Demo } from "./Demo";
 export const Court: FC = memo(() => {
   return (
     <div>
-      <div className="bg-gray-200 sm:px-48 px-8 h-screen pt-4">
+      <div className="bg-gray-200 sm:px-48 px-8 h-screen pt-36">
         <div className='bg-lime-400 sm:h-3/4 h-4/6 border-4 border-white grid grid-rows-4 grid-cols-12'>
             <div className='col-span-2 row-span-2 border-4 border-white'></div>
             <div className='col-span-8 row-span-1 border-4 border-white'></div>

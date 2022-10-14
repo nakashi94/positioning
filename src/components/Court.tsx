046ef958190e723e9b7a3a64f7dc53432da16d4c
nakashi94 <n0904yBUNJOU@gmail.com>
2022-10-14 23:37:24 +0900

    first commit!!

---
+++ src/components/Court.tsx
@@ -0,0 +1,9 @@
+import { FC, memo } from "react";
+
+export const Court: FC = memo(() => {
+  return (
+    <div className="w-full bg-lime-400">
+      <div className='mx-auto'>court</div>
+    </div>
+  )
+})
